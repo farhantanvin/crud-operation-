56a198d6ef9a7766073eb5a584b3191cd32bc9ef
Farhan Tanvin <38113532+farhantanvin@users.noreply.github.com>
2019-10-24 02:36:48 +0600

    Add files via upload

---
+++ controllers/user.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var userModel = require('./../models/user-model');
+
+var router = express.Router();
+
+
+router.get('*', function(request, response, next){
+
+	if(request.cookies['username'] != null){
+		next();
+	}else{
+		response.redirect('/logout');
+	}
+
+});
+
+router.get('/adduser', function(request, response){
+	response.render("user/adduser");
+});
+
+router.get('/userList', function(request, response){
+		
+		userModel.getAll(function(results){
+			response.render('user/index', {users: results});		
+		});	
+});
+
+router.get('/edit/:id', function(request, response){
+
+	userModel.getById(request.params.id, function(result){
+		response.render('user/edit', result);
+	});
+	
+});
+
+router.post('/edit/:id', function(request, response){
+
+	var user = {
+		username: request.body.username,
+		password: request.body.password,
+		id: request.params.id
+	};
+
+	userModel.update(user, function(status){
+		
+		if(status){
+			response.redirect('/user/userlist');
+		}else{
+			response.redirect('/user/edit/'+request.params.id);
+		}
+	});
+	
+});
+
+router.get('/delete/:id', function(request, response){
+
+	userModel.getById(request.params.id, function(result){
+		response.render("user/delete", result);
+	})
+});
+
+router.post('/delete/:id', function(request, response){
+
+	userModel.delete(request.params.id, function(status){	
+		if(status){
+			response.redirect("/user/userList");
+		}else{
+			response.redirect("/user/delete/"+request.params.id);	
+		}
+	})
+});
+
+
+router.post('/adduser', function(request, response){
+
+var user = {
+		username: request.body.username,
+		password: request.body.password,
+		
+	};
+
+
+	userModel.insert(user, function(status){	
+		if(status){
+			response.redirect("/user/userList");
+		}else{
+			response.redirect("/user/adduser");	
+		}
+	});
+});
+
+
+
+
+
+
+router.get('/details/:id', function(request, response){
+
+	userModel.getById(request.params.id, function(result){
+		response.render("user/details", result);
+	})
+});
+
+module.exports = router;
+
+
+
